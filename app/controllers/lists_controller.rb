b7da54aacf444a8fe7ef8252e6b149964c50db0a
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2023-08-21 13:57:05 +0000

    [Add]4章終了”

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,14 +8,15 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # 3. データをデータベースに保存する為のsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
-  
+
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
