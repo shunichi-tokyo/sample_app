60010fa6a5019799f423e894e4cb05787fdf26f3
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2023-08-23 14:44:48 +0000

    [Add] 7章終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/edit'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get '/top' => 'homes#top'
+
+  get 'top' => 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
